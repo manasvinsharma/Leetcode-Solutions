4da07fe3623787da82ac7f392b25dfc604c89038
manasvinsharma <55654162+manasvinsharma@users.noreply.github.com>
2022-03-10 22:51:18 +0530

    Time: 410 ms (55.09%), Space: 134 MB (37.06%) - LeetHub

--- 239-sliding-window-maximum/239-sliding-window-maximum.cpp
+++ 239-sliding-window-maximum/239-sliding-window-maximum.cpp
@@ -1,3 +1,8 @@
+// Think for this testcase
+// [1,3,-1,-3,5,3,6,7]
+// k = 3
+// ans = [3,3,5,5,6,7]
+
 class Solution {
 public:
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
@@ -5,6 +10,7 @@ public:
         deque<pair<int,int>>q;
         int n = nums.size();
         
+        // create a window of size k
         for(int i=0;i<k;i++)
         {
             if(q.empty())
@@ -29,6 +35,7 @@ public:
         
         ans.push_back(q.front().first);
         
+        // start moving the window by 1 
         for(int i=k;i<n;i++)
         {
             if(q.front().first==nums[i-k] && q.front().second==(i-k))
