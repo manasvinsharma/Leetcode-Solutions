7bfa0353d98fefbed8a9a74e93ef8566fdd872f2
manasvinsharma <55654162+manasvinsharma@users.noreply.github.com>
2022-07-10 18:05:39 +0530

    Added solution - LeetHub

---
+++ Largest number in K swaps - GFG/largest-number-in-k-swaps.cpp
@@ -0,0 +1,71 @@
+// { Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+ // } Driver Code Ends
+
+
+class Solution
+{
+    public:
+    string ans;
+    
+    void solve(string &str, int k, int curr)
+    {
+        int n = str.size();
+        
+        if(str > ans)
+        ans = str;
+        
+        if(k==0 || curr==n)
+        return ;
+        
+        if(curr!=0 && ans[curr-1]>str[curr-1])
+        return ;
+        
+        for(int i=curr;i<n;i++)
+        {
+            swap(str[i], str[curr]);
+            
+            if(curr!=i)
+            solve(str, k-1, curr+1);
+            else
+            solve(str, k, curr+1);
+            
+            swap(str[i], str[curr]);
+        }
+        return ;
+    }
+    
+    //Function to find the largest number after k swaps.
+    string findMaximumNum(string &str, int k)
+    {
+       // code here
+       int n = str.size();
+       
+       for(int i=0;i<n;i++)
+       ans+="0";
+       
+       solve(str, k, 0);
+       return ans;
+    }
+};
+
+// { Driver Code Starts.
+
+int main()
+{
+    int t, k;
+    string str;
+
+    cin >> t;
+    while (t--)
+    {
+        cin >> k >> str;
+        Solution ob;
+        cout<< ob.findMaximumNum(str, k) << endl;
+    }
+    return 0;
+}
+  // } Driver Code Ends
